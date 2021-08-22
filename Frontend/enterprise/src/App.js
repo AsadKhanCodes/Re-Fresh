a46cc60e21ff9a0944d3e59689756ebd29e6bd8b
Asad Khan <asad.khan@tutanota.com>
2021-08-21 21:54:50 -0400

    Create Grid Layout for Enterprise Dashboard

--- Frontend/enterprise/src/App.js
+++ Frontend/enterprise/src/App.js
@@ -1,11 +1,36 @@
 import React from 'react'
 import './App.css'
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Paper } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    grid: {
+        width: '100%',
+        margin: '0px',
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+        background: theme.palette.success.light
+    }
+}));
 
 function App() {
+    const classes = useStyles();
     return (
-        <div className='App'>
-            <h1>Hello World</h1>
-        </div>
+        <Grid container spacing = {5} className={classes.grid}>
+            <Grid item xs={12}>
+                <Paper className= {classes.paper}>Optimized Sales and Future Orders</Paper>
+            </Grid>
+            <Grid item xs={6}>
+                <Paper className= {classes.paper}>Stock</Paper>
+            </Grid>
+            <Grid item xs={6}>
+                <Paper className= {classes.paper}>Unit Sales</Paper>
+            </Grid>
+        </Grid>
+        
     )
 }
 
